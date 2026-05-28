f3844dbea04c58d05a6345ba0cc459bf7bd71618
Savan Shah <sas062@ucsd.edu>
2026-05-28 14:44:53 -0700

    Log

--- data_conversion.py
+++ data_conversion.py
@@ -1,16 +1,14 @@
 import pandas as pd
 
-def load_data(file_path):
-    try:
-        data = pd.read_csv(file_path)
-        return data
-    except Exception as e:
-        print(f"Error loading data: {e}")
-        return None
+
 
 def main():
-    bc_meta = 'data/validation_bc_meta.csv'
-    normal_meta = 'data/validation_normal_data.csv'
+    bc_meta = pd.read_csv('data/validation_bc_meta.csv')
+    normal_meta = pd.read_csv('data/validation_normal_data.csv')
+    pnas_normal_readcounts = pd.read_csv('data/reads/pnas_normal_readcounts.txt', sep='\t')
+    pnas_normal_tpm = pd.read_csv('data/reads/pnas_normal_tpm.txt', sep='\t')
+    pnas_readcounts_96 = pd.read_csv('data/reads/pnas_readcounts_96.txt', sep='\t')
+    pnas_tpm_96 = pd.read_csv('data/reads/pnas_tpm_96.txt', sep='\t')
 
 if __name__ == "__main__":
     main()    
